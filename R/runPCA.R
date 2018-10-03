15c8e577782f65d67837176af9aa4324d6852bb6
dhafer malouche <dhafer.malouche@yale.edu>
2018-10-03 17:02:47 +0100

    Add files via upload

--- R/runPCA.R
+++ R/runPCA.R
@@ -1,3 +1,16 @@
+#' @title A Shiny App for PCA
+#'
+#' @description This package contains a shiny app that helps to perform a PCA without any coding and where you can download every obtained results: figures, tables...
+#'
+#' @param void
+#'
+#' @return run a Shiny app
+#'
+#' @examples runPCA()
+#'
+#' @export
+
 runPCA<-function(){
+  library(shiny)
   runApp("R/")
 }
